6773cbd4fcccafcb6865bd13e032da8184a348e1
eugeneyan <eugeneyanziyou@gmail.com>
2024-09-01 22:01:04 -0700

    Complete nextjs chapter 6

---
+++ intros/nextjs/nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';    
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
